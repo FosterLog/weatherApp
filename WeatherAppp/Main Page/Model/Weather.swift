b96da7996428651d6408f6006bcc5e45aab1981d
Express Delivery <expressnerds@optus.com.au>
2018-09-25 16:43:42 +1000

    Joined method

--- WeatherAppp/Main Page/Model/Weather.swift
+++ WeatherAppp/Main Page/Model/Weather.swift
@@ -9,7 +9,10 @@ import Foundation
 
 var cityIds = ["4163971","2147714","2174003"]
 
+var weatherData: [List]?
+
 private var cityId: String = ""
+typealias DownloadComplete = () -> ()
 
 struct Weather: Codable {
     let cnt: Int
@@ -32,8 +35,22 @@ struct List: Codable {
 }
 
 func cityIdList() -> URL {
-    for each in cityIds {
-        cityId = "\(cityId),\(each)"
+    let cityId = cityIds.joined(separator: ",")
+    return URL(string: "https://api.openweathermap.org/data/2.5/group?id=\(cityId)&units=metric&appid=77479295e2ec74ed0b40a6d64b4bac2d")!
+}
+
+func downloadData(completed: @escaping DownloadComplete) {
+    let downloadURL = cityIdList()
+    let task = URLSession.shared.dataTask(with: downloadURL) { (data, urlResponse, error) in
+        guard let data = data, error == nil, urlResponse != nil else { return }
+        do {
+            let decoder = JSONDecoder()
+            let weather = try decoder.decode(Weather.self, from: data)
+            weatherData = weather.list
+            completed()
+        } catch {
+            print("Json error")
+        }
     }
-    return URL(string: "https://api.openweathermap.org/data/2.5/group?id=\(cityId.dropFirst())&units=metric&appid=77479295e2ec74ed0b40a6d64b4bac2d")!
+    task.resume()
 }
